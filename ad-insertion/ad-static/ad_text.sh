8e47b22a974951d879afa565ff5df8d8a4c099d5
Pengfei Qu <pengfei.qu@intel.com>
2019-06-24 17:25:40 +0800

    add the script to generate the ad clip with blue border and white text

---
+++ ad-insertion/ad-static/ad_text.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+IMAGE="ssai_content_provider_archive"
+DIR=$(dirname $(readlink -f "$0"))
+clips=()
+
+case "$(cat /proc/1/sched | head -n 1)" in
+*ad_text.sh*)
+    for clip in `find /bak -name "*.mp4" -print`; do
+        clip_name="${clip/*\//}"
+        echo $clip_name
+        if test ! -f "/archive/$clip_name"; then
+            ffmpeg -i "/bak/$clip_name" -vf "scale=1920:960,pad=1920:1080:0:60:blue,drawtext=text='Server-Side AD Insertion':x=(w-text_w)/2:y=30:fontsize=30:fontcolor=white" -y "/archive/$clip_name"
+        fi
+    done
+    wait
+    ;;
+*) 
+    mkdir -p "$DIR/../../volume/ad/archive"
+    mkdir -p "$DIR/../../volume/ad/bak"
+    . "$DIR/../../script/build.sh"
+    . "$DIR/shell.sh" /home/ad_text.sh $@
+    ;;
+esac
