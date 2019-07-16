3af04f63c93fca9a190a1419c9cb5b7533c0e114
Pengfei Qu <pengfei.qu@intel.com>
2019-07-16 03:18:02 -0400

    add the border for ad clip
    Signed-off-by: Pengfei Qu <pengfei.qu@intel.com>

--- ad-insertion/ad-static/shell.sh
+++ ad-insertion/ad-static/shell.sh
@@ -2,6 +2,6 @@
 
 IMAGE="ssai_ad_insertion_ad_static"
 DIR=$(dirname $(readlink -f "$0"))
-OPTIONS=("--volume=$DIR/../../volume/ad/static:/mnt:rw" "--volume=$DIR:/home:ro")
+OPTIONS=("--volume=$DIR/../../volume/ad/static:/mnt:rw" "--volume=$DIR:/home:ro" "--volume=$DIR/../../volume/ad/demo:/demo:rw" "--volume=$DIR/../../volume/ad/archive:/archive:rw")
 
 . "$DIR/../../script/shell.sh"
